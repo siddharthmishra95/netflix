840ea08701a1f54564f106eacab14ff48841825e
siddharthmishra95 <siiid.mishra@gmail.com>
2021-09-18 13:30:54 +0530

    Added Video and Hover Effect

--- client/src/components/listItem/ListItem.jsx
+++ client/src/components/listItem/ListItem.jsx
@@ -1,9 +1,50 @@
-import "./ListItem.scss"
+import "./ListItem.scss";
+import {
+    PlayArrow,
+    Add,
+    ThumbUpAltOutlined,
+    ThumbDownOutlined,
+} from "@material-ui/icons";
+import { useState } from "react";
 
-export default function ListItem() {
+export default function ListItem({ index }) {
+    const [isHovered, setIsHovered] = useState(false);
+    const trailer =
+        "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
     return (
-        <div className="listItem">
-            Item
+        <div
+            className="listItem"
+            style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <img
+                src="https://img1.hotstarext.com/image/upload/f_auto,t_web_hs_3x/sources/r1/cms/prod/old_images/MOVIE/5385/1770015385/1770015385-h"
+                alt=""
+            />
+            {isHovered && (
+                <>
+                    <video src={trailer} autoPlay={true} loop />
+                    <div className="itemInfo">
+                        <div className="icons">
+                            <PlayArrow className="icon" />
+                            <Add className="icon" />
+                            <ThumbUpAltOutlined className="icon" />
+                            <ThumbDownOutlined className="icon" />
+                        </div>
+                        <div className="itemInfoTop">
+                            <span>1 hour 14 mins</span>
+                            <span className="limit">+16</span>
+                            <span>2015</span>
+                        </div>
+                        <div className="desc">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                            Praesentium hic rem eveniet error possimus, neque ex doloribus.
+                        </div>
+                        <div className="genre">Action</div>
+                    </div>
+                </>
+            )}
         </div>
-    )
+    );
 }
