4549683cde7ef1ca7939bcc369615b7ff29eb5cc
siddharthmishra95 <siiid.mishra@gmail.com>
2021-09-19 00:58:42 +0530

    Login With JWT Token

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 // REGISTER 
 router.post("/register", async (req,res) => {
@@ -14,6 +15,34 @@ router.post("/register", async (req,res) => {
         const user = await newUser.save();
         res.status(201).json(user);
     }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+// LOGIN
+
+router.post("/login",async (req,res) => {
+    try {
+        const user = await User.findOne({email:req.body.email});
+        !user && res.status(401).json("Wrong password or Username!");
+
+
+        const bytes  = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+        originalPassword !== req.body.password &&
+        res.status(401).json("Wrong password or Username!");
+
+        const accessToken = jwt.sign(
+            {id: user._id, isAdmin: user.isAdmin },
+            process.env.SECRET_KEY,{expiresIn:"5d"}
+        );
+
+
+        const {password, ...info} = user._doc;
+
+        res.status(200).json({ ...info, accessToken });
+    } catch(err) {
         res.status(500).json(err);
     }
 });
---
+++ api/routes/users.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+
+
+// UPDATE
+
+// DELETE
+
+// GET
+
+// GET ALL
+
+// GET USER STATS
