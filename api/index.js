8160b5b0a322c4915c610c0fae87383fb97e3ba9
siddharthmishra95 <siiid.mishra@gmail.com>
2021-09-28 10:48:57 +0530

    Added REST API Movie Lists

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
 
 dotenv.config();
 
@@ -14,12 +15,14 @@ mongoose.connect(process.env.MONGO_URL, {
    
 })
 .then(()=>console.log("DB Connection Successfull!!!!!"))
+
 .catch((err)=> console.log(err));
 
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
 app.listen(8800, ()=>{
     console.log("Backend Server is Running!!! ")
