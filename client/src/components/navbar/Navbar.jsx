75f4c044ee411a456fc0f44eb72a750b050c4a54
siddharthmishra95 <siiid.mishra@gmail.com>
2021-09-28 11:20:54 +0530

    Implemented React Router Dom

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import "./navbar.scss"
 import { useState } from 'react'
 import { Search, Notifications, ArrowDropDown } from "@material-ui/icons"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
     const [isScrolled, setIsScrolled] = useState(false)
@@ -15,9 +16,15 @@ const Navbar = () => {
             <div className="container">
                 <div className="left">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/6/67/NewNetflixLogo.png" alt="" />
-                    <span>Homepage</span>
-                    <span>Series</span>
-                    <span>Movies</span>
+                    <Link to="/" className="link">
+                        <span>Homepage</span>
+                    </Link>
+                    <Link to="/series" className="link">
+                        <span>Series</span>
+                    </Link>
+                    <Link to="/movies" className="link">
+                        <span>Movies</span>
+                    </Link>
                     <span>New and Popular</span>
                     <span>My List</span>
                 </div>
